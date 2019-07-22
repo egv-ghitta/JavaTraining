12ffc8adecb615f20f83acbb6295c74440006050
egv-ghitta <emilghitta@gmail.com>
2019-07-22 18:38:05 +0300

    Addding Constructori

--- Stuff/src/Constructorii.java
+++ Stuff/src/Constructorii.java
@@ -4,7 +4,7 @@ public class Constructorii {
  * Constructorul este un block de cod , similar unei metode.
  * Este invocat cand o instanta a unui obiect este creata.
  * 
- * Reguli de creare a constructorului:
+ * Regulile de creare a constructorului:
  * 1. Numele constructorului trebuie sa fie acelasi cu numele clasei
  * 2. Trebuie sa nu aiba un return type explicit
  * 3. Nu poate sa fie abstract, static sau final
