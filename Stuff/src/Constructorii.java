acd98996baa89cc9fe79756563912ad3d0c9fb16
egv-ghitta <emilghitta@gmail.com>
2019-07-22 18:18:50 +0300

    Adding basic [Constructor class]

---
+++ Stuff/src/Constructorii.java
@@ -0,0 +1,18 @@
+
+public class Constructorii {
+/*
+ * Constructorul este un block de cod , similar unei metode.
+ * Este invocat cand o instanta a unui obiect este creata.
+ * 
+ * Reguli de creare a constructorului:
+ * 1. Numele constructorului trebuie sa fie acelasi cu numele clasei
+ * 2. Trebuie sa nu aiba un return type explicit
+ * 3. Nu poate sa fie abstract, static sau final
+ * 
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
